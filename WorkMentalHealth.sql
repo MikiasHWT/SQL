30bdc0e442b1ef3d3e8d58954b3cc733b922eea2
Mikias Woldetensae <165864268+MikiasHWT@users.noreply.github.com>
2024-09-29 17:38:20 -0700

    Update WorkMentalHealth.sql

--- WorkMentalHealth.sql
+++ WorkMentalHealth.sql
@@ -6,3 +6,53 @@ FROM impact_of_remote_work_on_mental_health;
 -- View Table
 SELECT *
 FROM work_mh;
+
+# 
+SELECT Job_Role, Work_Location, AVG(Hours_Worked_Per_Week) AS weekly
+FROM work_mh
+GROUP BY Job_Role, Work_Location
+ORDER BY Job_Role, Work_Location, weekly DESC;
+
+# Onsite job satisfaction by role
+SELECT Job_Role, AVG(Work_Life_Balance_Rating) AS Happiness
+FROM work_mh
+WHERE Work_Location = "Onsite"
+GROUP BY Job_Role
+ORDER BY Happy ASC;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
