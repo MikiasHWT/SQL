81f1d8b61b2afd2b93adb1d43295cc1a5fd164df
Mikias Woldetensae <165864268+MikiasHWT@users.noreply.github.com>
2024-10-01 16:44:30 -0700

    VScode Instance

--- WorkMentalHealth.sql
+++ WorkMentalHealth.sql
@@ -7,18 +7,19 @@ FROM impact_of_remote_work_on_mental_health;
 SELECT *
 FROM work_mh;
 
-# 
+-- 
 SELECT Job_Role, Work_Location, AVG(Hours_Worked_Per_Week) AS weekly
 FROM work_mh
 GROUP BY Job_Role, Work_Location
 ORDER BY Job_Role, Work_Location, weekly DESC;
 
-# Onsite job satisfaction by role
+-- Onsite job satisfaction by role
 SELECT Job_Role, AVG(Work_Life_Balance_Rating) AS Happiness
 FROM work_mh
 WHERE Work_Location = "Onsite"
 GROUP BY Job_Role
 ORDER BY Happy ASC;
+
 
 
 
